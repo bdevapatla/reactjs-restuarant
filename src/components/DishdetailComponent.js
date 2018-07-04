3e802765607890652bc4f96ca5ac00c1e90a2dd9
bdevapatla <reachbp4@gmail.com>
2018-07-04 16:52:51 -0400

    react-animation-components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import {Link} from 'react-router-dom';
 import {Loading} from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import {FadeTransform,Fade,Stagger} from 'react-animation-components'
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -109,13 +110,18 @@ class CommentForm extends Component {
 
 function RenderDish({dish}) {
     return (
+        <FadeTransform in transformProps={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+            }}>
         <Card>
             <CardImg top src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
             </CardBody> 
-        </Card>);
+        </Card>
+        </FadeTransform>
+        );
 }
 
 
@@ -123,17 +129,21 @@ function RenderComments({comments,postComment,dishId}) {
     if(comments != null) {
         const mappedComments =  comments.map(comment => {
                             return (
+                                <Fade in>
                                 <li key={comment.id}>
                                     {comment.comment} <br/><br/>
                                     --{comment.author} {new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(comment.date)))} <br/> <br/>
                                 </li> 
+                                </Fade>
                             )
                         });
         return (
                 <div>
                 <h4>Comments</h4>
                 <ul className="list-unstyled">
-                    {mappedComments}
+                    <Stagger in>
+                        {mappedComments}
+                    </Stagger>
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment} />
                 </div>);
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle} from 'reactstrap';
 import {Loading} from './LoadingComponent'
 import { baseUrl } from '../shared/baseUrl';
-
+import {FadeTransform} from 'react-animation-components'
 
 function RenderCard({item,isLoading,errMess}) {
     if(isLoading) {
@@ -17,6 +17,9 @@ function RenderCard({item,isLoading,errMess}) {
     }
     else {
         return (
+            <FadeTransform in transformProps={{
+                exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
             <Card>
                 <CardImg src={baseUrl + item.image} alt={item.name} />
                 <CardBody>
@@ -25,6 +28,7 @@ function RenderCard({item,isLoading,errMess}) {
                     <CardText>{item.description}</CardText>
                 </CardBody>
             </Card>
+            </FadeTransform>
         );
     }
 }
